642364a6c9673e823f4a4c3c3c4b677aa641c79a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-10 02:23:49 -0500

    commit tree dd58a7351ac00bdf45bf586b00adca7312ce2533

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Mr. Chocolate", 5, "M", "brown", "cool", true);
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Rowdy", 1, "F", "yellow", "hyper", true );
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Tabitha", 5, "M", "brown", "cool", true);
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Sergeant Brown", 1, "F", "yellow", "hyper", true );
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Melissa", 5, "M", "brown", "cool", true);
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Grinch", 1, "F", "yellow", "hyper", true );
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( "Wendy", 5, "M", "brown", "cool", true);
+
+INSERT INTO bears (name, age , sex, color, temperament, alive)
+VALUES( NULL, 1, "F", "yellow", "hyper", true );
